1b581249f52c691b1384d0b4becffc5867ead287
deAndi <andreas.schmieder@googlemail.com>
2017-08-28 00:55:16 +0200

    Authentification with Facebook integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,38 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from "../pages/login/login";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyB9jmjNp6TGh9WaEahZx2BeUVC6x1Ugz4k",
+      authDomain: "wamb-d43a5.firebaseapp.com",
+      databaseURL: "https://wamb-d43a5.firebaseio.com",
+      projectId: "wamb-d43a5",
+      storageBucket: "wamb-d43a5.appspot.com",
+      messagingSenderId: "342202611139"
+    });
+
+    const unsubscribe = firebase.auth().onAuthStateChanged((user)=>{
+      if (!user){
+        this.rootPage = LoginPage;
+        unsubscribe();
+      } else {
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    })
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, IonicPageModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Facebook } from '@ionic-native/facebook';
+import firebase from 'firebase';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AuthProvider } from '../providers/auth/auth';
+import { LoginPage } from "../pages/login/login";
 
+export const firebaseConfig = {
+  apiKey: "yourApiKey",
+  authDomain: "takeItFromFirebase",
+  databaseURL: "AllFromFirebase",
+  projectId: "Id and So",
+  storageBucket: "Aha Aha Aha",
+  messagingSenderId: "FirebaseID"
+}
+firebase.initializeApp(firebaseConfig);
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicPageModule.forChild(LoginPage),
+    
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+      
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Facebook,
+    AuthProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    
   ]
 })
 export class AppModule {}
